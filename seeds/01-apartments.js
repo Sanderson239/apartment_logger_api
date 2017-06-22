5c737cc377da967e1099fe3c24bfd3a44d61da50
Stephen Anderson <sa3196a@student.american.edu>
2017-06-21 23:28:21 -0700

    fixed testing errors

--- seeds/01-apartments.js
+++ seeds/01-apartments.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('apartments').del()
     .then( () => {
       // Inserts seed entries
       return knex('apartments').insert([
